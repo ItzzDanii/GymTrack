3b5464767f70b13bafc54a8125d3ae985dd5e59b
DESKTOP-GAODOHB\HPM <barbuto.daniel29@hotmail.com>
2026-06-08 10:09:11 +0200

    Aggiunto una riga di pulsanti shortcut sotto titolo di home e aggiunte funzioni per sezione profile etc

--- pages/home_script.js
+++ pages/home_script.js
@@ -1,10 +1,13 @@
+let scenari = [];
+
+function inizializzaScenari() {
+    scenari = [document.getElementById("main-container"), document.getElementById("profile-container")];
+}
+
 function changeTheme() {
     let body = document.body;
-
     body.classList.toggle("dark-mode");
-
     let themeIcon = document.getElementById("change-theme");
-
     if (body.classList.contains("dark-mode")) {
         themeIcon.classList.remove("fa-sun");
         themeIcon.classList.add("fa-moon");
@@ -20,31 +23,54 @@ function logout() {
 }
 
 function controllaLoggato() {
-    if (sessionStorage.getItem("loggato") !== "true")
+    if (sessionStorage.getItem("loggato") !== "true") {
         window.location.href = '/index.html';
+    } else {
+        inizializzaScenari();
+        showUsername();
+        setInterval(showUsername, 1);
+    }
 }
 
-function loadProfile(){
-    document.getElementById("profile-email").value = sessionStorage.getItem("email");
+function setScenario(scenario) {
+    for (let i = 0; i < scenari.length; i++) {
+        if (scenari[i].id != scenario) {
+            document.getElementById(scenari[i].id).style.display = 'none';
+        } else {
+            document.getElementById(scenari[i].id).style.display = 'block';
+        }
+    }
+}
+
+function showUsername() {
+    let username = sessionStorage.getItem("username");
+    if (username) {
+        document.getElementById("show-username").innerText = username;
+    }
+}
+
+function loadProfile() {
+    document.getElementById("profile-email").value = sessionStorage.getItem("email") || "";
+    let inputUsername = document.getElementById("profile-username");
+    if (inputUsername && inputUsername.value.trim() !== "") {
+        sessionStorage.setItem("username", inputUsername.value);
+    }
 }
 
 function openProfile() {
-    document.getElementById("main-container").style.display = 'none';
-    document.getElementById("profile-container").style.display = 'block';
+    setScenario("profile-container");
     loadProfile();
 }
 
 function openHome() {
-    document.getElementById("main-container").style.display = 'block';
-    document.getElementById("profile-container").style.display = 'none';
+    setScenario("main-container");
 }
 
 function changePic() {
     let input = document.createElement('input');
     input.type = 'file';
     input.accept = 'image/png';
-    
-    input.onchange = function(e) {
+    input.onchange = function (e) {
         let file = e.target.files[0];
         if (file) {
             let url = URL.createObjectURL(file);
@@ -53,6 +79,5 @@ function changePic() {
             });
         }
     };
-    
     input.click();
 }
