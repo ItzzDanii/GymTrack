7658ce6add1cc3cafa7fae7f0b82b3a058d7b13c
DESKTOP-GAODOHB\HPM <barbuto.daniel29@hotmail.com>
2026-06-08 15:42:52 +0200

    Aggiunta sezione Allenamento con pulsante che permette di avviare un allenamento; l'allenamento prevede un timer,numero del volume totale e numero dei set. Inoltre sono presenti alcuni pulsanti: uno per un timer,uno per aprire spotify e uno per terminare l'allenamento

--- pages/home_script.js
+++ pages/home_script.js
@@ -1,7 +1,15 @@
 let scenari = [];
+let workout = false;
+let timerInterval = null;
+let uiInterval = null;
 
 function inizializzaScenari() {
-    scenari = [document.getElementById("main-container"), document.getElementById("profile-container")];
+    scenari = [
+        document.getElementById("main-container"),
+        document.getElementById("profile-container"),
+        document.getElementById("startworkout-container"),
+        document.getElementById("workout-container")
+    ];
 }
 
 function changeTheme() {
@@ -30,6 +38,7 @@ function controllaLoggato() {
         loadProfile();
         showUsername();
         setInterval(showUsername, 500);
+        setScenario("main-container");
     }
 }
 
@@ -70,8 +79,6 @@ function loadProfile() {
     if (follows) {
         follows.textContent = localStorage.getItem("follows_" + emailSalvata) || "0";
     }
-
-    sessionStorage.getItem("workouts_" + emailSalvata) === 0;
 
     let inputUsername = document.getElementById("profile-username");
     let inputInstagram = document.getElementById("profile-instagram");
@@ -118,6 +125,11 @@ function openHome() {
     setScenario("main-container");
 }
 
+function openWorkout() {
+    resetInfo();
+    setScenario("startworkout-container");
+}
+
 function changePic() {
     let emailSalvata = sessionStorage.getItem("email") || "";
     if (emailSalvata === "") return;
@@ -144,12 +156,12 @@ function changePic() {
 
 function openInsta() {
     let username = document.getElementById("profile-instagram").value.trim();
-    
+
     if (username !== "") {
         if (username.startsWith("@")) {
             username = username.substring(1);
         }
-        
+
         let url = "https://www.instagram.com/" + username;
         window.open(url, '_blank');
     } else {
@@ -157,12 +169,103 @@ function openInsta() {
     }
 }
 
-// DEBUG
+function openSpotify() {
+    let url = "https://open.spotify.com/intl-it/";
+    window.open(url, '_blank');
+}
 
+function resetInfo() {
+    let timer = '0:00:00';
+    let volume = 0;
+    let set = 0;
+
+    document.getElementById("duration-value").textContent = timer;
+    document.getElementById("volume-value").innerText = volume + 'kg';
+    document.getElementById("set-value").innerText = set;
+}
+
+function endWorkout() {
+    workout = false;
+
+    let emailSalvata = sessionStorage.getItem("email") || "";
+    let setCorrenti = document.getElementById("set-value").innerText;
+
+    if (parseInt(setCorrenti) > 0 && setCorrenti.trim() !== "") {
+
+        if (sessionStorage.getItem("id_workout") == null) {
+            sessionStorage.setItem("id_workout", 1);
+        }
+        let currentId = sessionStorage.getItem("id_workout");
+
+        let durataCorrente = document.getElementById("duration-value").textContent;
+        let volumeCorrente = document.getElementById("volume-value").innerText;
+
+        let datiWorkout = `set:${setCorrenti}_duration:${durataCorrente}_volume:${volumeCorrente}`;
+
+        sessionStorage.setItem(emailSalvata + "_workout_" + currentId, datiWorkout);
+        localStorage.setItem("workouts_" + emailSalvata, currentId);
+        sessionStorage.setItem("id_workout", parseInt(currentId) + 1);
+    }
+
+    if (timerInterval) clearInterval(timerInterval);
+    if (uiInterval) clearInterval(uiInterval);
+
+    resetInfo();
+
+    let btnStart = document.getElementById("btnStart");
+    if (btnStart) {
+        btnStart.style.display = 'block';
+    }
+
+    setScenario("startworkout-container");
+}
+
+function startWorkout() {
+    if (workout === false) {
+        setScenario("workout-container");
+        workout = true;
+        document.getElementById("btnStart").style.display = 'none';
+
+        let sec = 0;
+        let min = 0;
+        let h = 0;
+        let timer = '0:00:00';
+        document.getElementById("duration-value").textContent = timer;
+
+        timerInterval = setInterval(() => {
+            sec++;
+            if (sec >= 60) {
+                sec = 0;
+                min++;
+            }
+            if (min >= 60) {
+                min = 0;
+                h++;
+            }
+
+            let formattedH = String(h).padStart(1, '0');
+            let formattedM = String(min).padStart(2, '0');
+            let formattedS = String(sec).padStart(2, '0');
+
+            let timer = `${formattedH}:${formattedM}:${formattedS}`;
+            document.getElementById("duration-value").textContent = timer;
+        }, 1000);
+
+        let volume = 0;
+        let set = 0;
+
+        uiInterval = setInterval(() => {
+            document.getElementById("volume-value").innerText = volume + 'kg';
+            document.getElementById("set-value").innerText = set;
+        }, 200);
+    } else setScenario("startworkout-container");
+}
+
+// DEBUG
 function resetWork() {
     let emailSalvata = sessionStorage.getItem("email") || "";
     if (emailSalvata === "") return;
-    
+
     let element = document.getElementById("n-workout");
     if (element) {
         element.textContent = "0";
@@ -173,7 +276,7 @@ function resetWork() {
 function resetFlr() {
     let emailSalvata = sessionStorage.getItem("email") || "";
     if (emailSalvata === "") return;
-    
+
     let element = document.getElementById("n-follower");
     if (element) {
         element.textContent = "0";
@@ -184,7 +287,7 @@ function resetFlr() {
 function resetFlw() {
     let emailSalvata = sessionStorage.getItem("email") || "";
     if (emailSalvata === "") return;
-    
+
     let element = document.getElementById("n-follow");
     if (element) {
         element.textContent = "0";
